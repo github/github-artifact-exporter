1ec699ff77ef52f2783b562ebb6978f0b81ed330
Froilan Irizarry Rivera <1918027+froi@users.noreply.github.com>
2020-09-03 20:40:04 -0400

    Fix typos
    A couple of typos got through after a massive replace. These have been fixed.
    
    Signed-off-by: Froilan Irizarry Rivera <1918027+froi@users.noreply.github.com>

--- packages/core/github.d.ts
+++ packages/core/github.d.ts
@@ -4439,7 +4439,7 @@ export type GitActorAvatarUrlArgs = {
 export type GitHubMetadata = {
   __typename?: "GitHubMetadata";
   /** Returns a String that's a SHA of `github-services` */
-  gitHubServicesSha: Scalars["GitObjectID"];
+  githubervicesSha: Scalars["GitObjectID"];
   /** IP addresses that users connect to for git operations */
   gitIpAddresses?: Maybe<Array<Scalars["String"]>>;
   /** IP addresses that service hooks are sent from */
